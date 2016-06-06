9b00e62a71162a4ea80493b204a80976c78e6df6
Hector Gonzalez <hector.glez4@gmail.com>
2016-06-06 17:14:08 +0200

    readme

--- public/js/vehicules.js
+++ public/js/vehicules.js
@@ -35,5 +35,36 @@ function loadClients()
 }
 createVehicule()
 {
-	
+
+}
+loadVehiculeBrand()
+{
+	URL = "/vehicule/client/";
+	URL +=  $("#client-select").val();
+	$.ajax({
+		url: URL,
+		type: 'get',
+		dataType: 'json',
+	}).done(function(data)
+	{
+		var sTable = "";
+
+		$.each(data, function(index, vehicule)
+		{
+			//alert(vehicule);
+			sTable += "<tr>";
+			sTable += "<td>"+ vehicule.VHC_ID +"</td>";
+			sTable += "<td>"+ vehicule.VHC_Registration +"</td>";
+			sTable += "<td>"+ vehicule.VHC_IDV +"</td>";
+			sTable += "<td>"+ "Brand" +"</td>";
+			sTable += "<td>"+ "Model" +"</td>";
+			sTable += "<td><div class='col-xs-6'><p class='text-center'data-placement='top' data-toggle='tooltip' title='Edit'>"+
+						"<button class='btn btn-primary btn-xs' data-title='Edit'><span class='glyphicon glyphicon-pencil'></span></button></p>"+
+						"</div><div class='col-xs-6'><p class='text-center' data-placement='top' data-toggle='tooltip' title='Delete'>"+
+						"<button class='btn btn-danger btn-xs' data-title='Delete'><span class='glyphicon glyphicon-trash'></span></button></p></div></td>";
+			sTable += "</tr>";
+		});
+		$("#tbVehicules").html(sTable);
+		$("#vehiculesFooter").removeClass("hidden");
+	})
 }
